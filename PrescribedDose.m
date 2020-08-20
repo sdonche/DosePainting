91d4ac67cae2d21c50b9b90c0e6982797a9fe5e2
Sam Donche <sampsc.donche@gmail.com>
2020-08-20 15:16:11 +0200

    Added formula and description to the function

--- PrescribedDose.m
+++ PrescribedDose.m
@@ -1,7 +1,28 @@
-function [outputArg1,outputArg2] = PrescribedDose(inputArg1,inputArg2)
-%UNTITLED4 Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+function PresDose = PrescribedDose(I,I_high,I_low,D_high,D_low)
+% PrescribedDose calculates the prescribed dose from a (PET) intensity.
+% In this method, the dose increases linearly with intensity between D(low) 
+% and D(high), respectively corresponding to I(low) and I(high).
+% This method was adopted from [18F]FDG PET voxel intensity-based IMRT for
+% head and neck cancer at Ghent University Hospital
+% (https://doi.org/10.1016/j.radonc.2006.03.003)
+% 
+% Formula:
+% D(I) = D(low) + (I - I(low))/(I(high) - I(low))*(D(high) - D(low))
+% 
+% with
+% I             = voxel intensity
+% D(I)          = prescribed dose
+% D(high)       = 28 Gy
+% D(low)        = 20 Gy
+% I(high)       = 95% of PET voxel intensity        
+% I(low)        = I(high)*0.25
+% 
+% TODO
+% To minimize the influence of PET signal noise on I(low) and I(high), the
+% dose was escalated between 25 and 100% of the 95th percentile PET voxel
+% intensity value within PTV(69+PET)
+
+PresDose = D_low + (I - I_low)/(I_high - I_low)*(D_high - D_low);
+
 end
 
