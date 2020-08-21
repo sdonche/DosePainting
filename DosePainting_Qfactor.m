2af46b4bd5dbc20b14d20e882208def13fe83c76
Sam Donche <sampsc.donche@gmail.com>
2020-08-21 13:22:03 +0200

    Addition of Q-factor and QVH calculation

--- DosePainting_Qfactor.m
+++ DosePainting_Qfactor.m
@@ -1,15 +1,127 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Q factor
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% 
-% For PTV(69+PET), the resulting dose distribution was compared to the
-% intended voxel intensity-based dose pattern; therefore, a quality factor
-% (QF) was introduced. In every point p theat is randomly seeded within
-% PTV(69+PET) (n points in total), the obtained over-intended dose ratio
-% Q(p) = D(p)/(D(I))(p) is calculated. This information can be visualized
+%
+% Information
+%
+% For the volume defined by > SUV50, the resulting dose distribution is 
+% compared to the intended voxel intensity-based dose pattern; therefore, 
+% a quality factor (QF) is introduced. For every voxel (n in total) in this 
+% volume, the obtained over-intended dose ratio is calculated. 
+%
+% This information can be visualized
 % in a Q-volume histogram (QVH), by displaying the partial PTV(69+PET)
 % volume for which Q is greater than or equal to each abscis value. Ideally
 % such a curve would drop steeply at Q = 1. QF is defined as the mean
-% absolute deviation of Q to 1 withing PTV(69+PET):
+% absolute deviation of Q to 1 within PTV(69+PET):
 %
-% QF = (1/n)*sum(p)abs(Q(p)-1)
+% QF = (1/n)*sum(p)abs(Q(p)-1) 
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% CLEAN SLATE
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+clearvars;
+close all;
+clc;
+imtool close all;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% MATLAB TOOLBOXES
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%   SPM12 (https://www.fil.ion.ucl.ac.uk/spm/software/download/)
+%   NIFTI and ANALYZE tools (https://nl.mathworks.com/matlabcentral/fileexchange/8797-tools-for-nifti-and-analyze-image)
+%   Medical Image Reader and Viewer (https://www.mathworks.com/matlabcentral/fileexchange/53745-medical-image-reader-and-viewer)
+%   nifti_utils (https://github.com/justinblaber/nifti_utils)
+
+% TODO update this in all scripts
+disp('Reading Toolboxes...')
+addpath('C:\Users\Hoofdgebruiker\OneDrive - UGent\Doctoraat\MATLAB\Extra\spm12')
+addpath('C:\Users\Hoofdgebruiker\OneDrive - UGent\Doctoraat\MATLAB\Extra\NIfTI_20140122')
+addpath('C:\Users\Hoofdgebruiker\OneDrive - UGent\Doctoraat\MATLAB\Extra\Medical Image Reader and Viewer')
+addpath('C:\Users\Hoofdgebruiker\OneDrive - UGent\Doctoraat\MATLAB\Extra\nifti_utils-master\nifti_utils') % "updated" version of NIfTI_20140122
+addpath('C:\Users\Hoofdgebruiker\OneDrive - UGent\Doctoraat\MATLAB\Preclinical\Scripts\App_BoundingBox')
+addpath('C:\Users\Hoofdgebruiker\OneDrive - UGent\Doctoraat\MATLAB\Preclinical\DosePainting\')
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% READ IMAGES
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Workdirectory
+cd 'D:\Desktop\PhD Meeting\Demo\';
+pathname = [pwd,'\'];
+
+% Coregistered Images
+pathname_coreg = [pathname, '5Coregister\'];
+scans_coreg = dir(pathname_coreg);
+scans_coreg = scans_coreg(3:end);
+
+% Load coregistered images
+disp(['Reading coregistered images from ',pathname_coreg,'...'])
+
+% CT
+    % Load
+    CT = niftiread(fullfile([pathname_coreg, scans_coreg(1).name]));
+    CT_info = niftiinfo(fullfile([pathname_coreg, scans_coreg(1).name]));
+% MRI
+    % Load
+    MRI = niftiread(fullfile([pathname_coreg, scans_coreg(4).name]));
+    MRI_info = niftiinfo(fullfile([pathname_coreg, scans_coreg(4).name]));
+% PET
+    % Load
+    PET = niftiread(fullfile([pathname_coreg, scans_coreg(5).name]));
+    PET_info = niftiinfo(fullfile([pathname_coreg, scans_coreg(5).name]));
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% READ DOSE MAPS
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Load ideal dose map
+load('IDM.mat')
+
+% Load SARRP dose map
+% DoseMap = niftiread(...
+% DoseMap_info = niftiinfo(...
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% CALCULATE Q-factor
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+Qp = zeros(nnz(IDM),1);
+counter = 1;
+
+for i = 1 : size(PET,1)
+    for j = 1 : size(PET,2)
+        for k = 1 : size(PET,3)
+        
+            IdealDose = IDM(i,j,k);
+            if IdealDose > 1
+                
+                ObtainedDose = DoseMap(i,j,k);
+                Qp(counter) = ObtainedDose/IdealDose;
+                counter = counter + 1;
+                
+            end
+            
+        end
+    end
+end
+    
+Q_factor = sum(abs(Qp -1))/length(Qp);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Q-VOLUME HISTOGRAM
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+Q_hist = 0:0.01:1.5;
+Vol_hist = zeros(1,length(Q_hist));
+
+for i = 1 : length(Q_hist)
+    
+    Q_val = Q_hist(i);
+    Vol_hist(i) = sum(Qp <= Q_val);
+    
+end
+
+scatter(Q_hist,Vol_hist)
