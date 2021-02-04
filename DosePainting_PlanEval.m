4f0e601623f24869ee75d66307b4b21d7f727cea
Sam Donche <69041142+sdonche@users.noreply.github.com>
2021-02-04 21:43:58 +0100

    Update DosePainting_PlanEval.m

--- DosePainting_PlanEval.m
+++ DosePainting_PlanEval.m
@@ -1,19 +1,19 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Q factor
+%% Plan Evaluation
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
 % Information
 %
-% For the volume defined by > SUV50, the resulting dose distribution is 
-% compared to the intended voxel intensity-based dose pattern; therefore, 
-% a quality factor (QF) is introduced. For every voxel (n in total) in this 
-% volume, the obtained over-intended dose ratio is calculated. 
+% This script generates a dose-volume histogram (DVH), Q-volume histogram
+% (QVH) and the Q-factor. These parameters can be used to evaluate the
+% radiation plan. 
 %
-% This information can be visualized
-% in a Q-volume histogram (QVH), by displaying the partial PTV(69+PET)
-% volume for which Q is greater than or equal to each abscis value. Ideally
-% such a curve would drop steeply at Q = 1. QF is defined as the mean
-% absolute deviation of Q to 1 within PTV(69+PET):
+% A quality index for each pixel Q(p) is obtained by dividing the obtained
+% dose over the intended dose. These values can be visualised in a Q-volume
+% histogram, by displaying the partial volume for which the QF is greater
+% than or equal to each avscis value. Ideally such a curve would drop
+% steeply at Q = 1. The quality-factor (QF) is defined as the mean absolute
+% deviation of Q to 1 within the given volume:
 %
 % QF = (1/n)*sum(p)abs(Q(p)-1)
 %
@@ -21,7 +21,7 @@
 % head and neck cancer at Ghent University Hospital
 % (https://doi.org/10.1016/j.radonc.2006.03.003)
 %
-% Conform PhD Sam Donche
+% Written by Sam Donche
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% CLEAN SLATE
@@ -33,19 +33,24 @@ clc;
 imtool close all;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% PARAMETERS TO ADJUST
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Work directory
+workdir = pwd;                  % Current MATLAB directory is important!!
+% or pathname = 'C:\...';
+
+% Directory with NIfTI file SARRP dose, called "SARRPDose.nii"
+dosedir = 'Method8+1';
+
+% Load extra files
+IDM_file = 'IDM.mat';
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% MATLAB TOOLBOXES
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %   SPM12 (https://www.fil.ion.ucl.ac.uk/spm/software/download/)
-%   NIFTI and ANALYZE tools (https://nl.mathworks.com/matlabcentral/fileexchange/8797-tools-for-nifti-and-analyze-image)
-%   Medical Image Reader and Viewer (https://www.mathworks.com/matlabcentral/fileexchange/53745-medical-image-reader-and-viewer)
-%   nifti_utils (https://github.com/justinblaber/nifti_utils)
-
-% TODO update this in all scripts
-disp('Reading Toolboxes...')
 addpath('C:\Users\Hoofdgebruiker\OneDrive - UGent\Doctoraat\MATLAB\Extra\spm12')
-addpath('C:\Users\Hoofdgebruiker\OneDrive - UGent\Doctoraat\MATLAB\Extra\NIfTI_20140122')
-addpath('C:\Users\Hoofdgebruiker\OneDrive - UGent\Doctoraat\MATLAB\Extra\Medical Image Reader and Viewer')
-addpath('C:\Users\Hoofdgebruiker\OneDrive - UGent\Doctoraat\MATLAB\Extra\nifti_utils-master\nifti_utils') % "updated" version of NIfTI_20140122
 addpath('C:\Users\Hoofdgebruiker\OneDrive - UGent\Doctoraat\MATLAB\Preclinical\Scripts\App_BoundingBox')
 addpath('C:\Users\Hoofdgebruiker\OneDrive - UGent\Doctoraat\MATLAB\Preclinical\DosePainting\')
 
@@ -54,7 +59,7 @@ addpath('C:\Users\Hoofdgebruiker\OneDrive - UGent\Doctoraat\MATLAB\Preclinical\D
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Workdirectory
-cd 'D:\Desktop\PhD Meeting\Demo\';
+cd(strcat(workdir,'\'));
 pathname = [pwd,'\'];
 
 % Coregistered Images
@@ -83,39 +88,32 @@ disp(['Reading coregistered images from ',pathname_coreg,'...'])
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Load SARRP dose map
-file_name_map = 'SARRPDose.nii';
+file_name_map = strcat(dosedir,'\SARRPDose.nii');
 X = ['Reading ', file_name_map,'...'];
 disp(X); clearvars X;
 DoseMap = niftiread(fullfile([pathname,file_name_map]));
 DoseMap_info = niftiinfo(fullfile([pathname,file_name_map]));
 
 % Load ideal dose map
-load('IDM.mat')
+load(IDM_file)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% REORIENT DOSEMAP
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Reorient dose map calculated by SARRP
-% TODO check if this is correct
 
 DoseMap_flipped = flip(DoseMap,2);
 
-% figure()
-% orthosliceViewer(flipped)
-% figure()
-% orthosliceViewer(DoseMap)
-% figure()
-% orthosliceViewer(IDM)
-% figure()
-% orthosliceViewer(CT)
-
-%     % Flip around Z-axis
-%     DoseMap_info.Transform.T(3,1:3) = -(CT_info.Transform.T(3,1:3));
-%     DoseMap_info.Transform.T(4,2) = -(CT_info.Transform.T(4,2)); % adjust translation along y-axis
-% 
-%     % Save new nifti file
-%     % Sform in header is automatically adjusted when saving
-%     niftiwrite(DoseMap,'SARRPDose_reorient.nii',DoseMap_info);
+if 0
+    figure()
+    orthosliceViewer(DoseMap_flipped)
+    figure()
+    orthosliceViewer(DoseMap)
+    figure()
+    orthosliceViewer(IDM)
+    figure()
+    orthosliceViewer(CT)
+end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% CALCULATE Q-factor
@@ -136,36 +134,120 @@ for i = 1 : size(PET,1)
                 counter = counter + 1;
                 
             end
-            
         end
     end
 end
     
 Q_factor = sum(abs(Qp -1))/length(Qp);
+display(Q_factor)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Q-VOLUME HISTOGRAM
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-Q_hist = 0:0.01:1.5;
+Q_hist = 0:0.001:1.5;
 Vol_hist = zeros(1,length(Q_hist));
 
 for i = 1 : length(Q_hist)
-    
     Q_val = Q_hist(i);
-    Vol_hist(i) = sum(Qp >= Q_val)/length(Qp)*100;
-    
+    Vol_hist(i) = sum(Qp >= Q_val)/length(Qp)*100;    
 end
 
-scatter(Q_hist,Vol_hist)
+hist = scatter(Q_hist,Vol_hist);
 xlabel('Q-value')
 ylabel('Volume (%)')
 
+saveas(hist,strcat(dosedir,'\Q-Volume_Histogram.fig'))
+saveas(hist,strcat(dosedir,'\Q-Volume_Histogram.jpg'))
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%
+%% DIFFERENCE MAP
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Extra info
+%       DoseMap too high --> negative value
+%       DoseMap too low  --> positive value
+
+Diff = IDM - DoseMap_flipped;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% FIGURES
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+if 0
+    figure()
+    orthosliceViewer(DoseMap_flipped)
+    colormap(jet)
 
+    figure()
+    orthosliceViewer(IDM)
+    colormap(jet)
 
-figure()
-orthosliceViewer(DoseMap)
+    figure()
+    orthosliceViewer(Diff)
+    colormap(jet)
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% SAVE NIFTI
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Files to view the ideal dose map in external software
+
+% Dose Map
+niftiwrite(DoseMap_flipped,strcat('DoseMap','_',dosedir,'.nii'),DoseMap_info)
+
+% Difference Map
+Diff16 = int16(Diff);
+niftiwrite(Diff16,strcat('DifferenceMap','_',dosedir,'.nii'),CT_info)
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% DOSE-VOLUME HISTOGRAM
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+Doses = zeros(nnz(IDM),1);
+counter = 1;
+
+for i = 1 : size(PET,1)
+    for j = 1 : size(PET,2)
+        for k = 1 : size(PET,3)
+        
+            IdealDose = IDM(i,j,k);
+            if IdealDose > 1
+                
+                Doses(counter) = DoseMap_flipped(i,j,k);
+                Ideal_Doses(counter) = IdealDose;
+                counter = counter + 1;
+                
+            end
+        end
+    end
+end
+
+D_hist = 0:0.01:3500;
+DVol_hist = zeros(1,length(D_hist));
+
+for i = 1 : length(D_hist)
+    
+    Dose = D_hist(i);
+    DVol_hist(i) = sum(Doses >= Dose)/length(Doses)*100;
+    
+end
+
+D_hist_IDM = 0:0.01:3500;
+DVol_hist_IDM = zeros(1,length(D_hist));
+
+for i = 1 : length(D_hist_IDM)
+    
+    Dose = D_hist_IDM(i);
+    DVol_hist_IDM(i) = sum(Ideal_Doses >= Dose)/length(Ideal_Doses)*100;
+    
+end
+
+hist2 = scatter(D_hist,DVol_hist);
+hold on
+scatter(D_hist_IDM,DVol_hist_IDM);
+xlabel('Dose')
+ylabel('Volume (%)')
+
+saveas(hist2,strcat(dosedir,'\Dose-Volume_Histogram.fig'))
+saveas(hist2,strcat(dosedir,'\Dose-Volume_Histogram.jpg'))
+
